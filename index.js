c0e27d13d393baff10ecf0ee84ddaea8e375838f
astro <angelica.strong0725@gmail.com>
2023-09-27 10:05:45 -0700

    writetofile function

--- index.js
+++ index.js
@@ -54,7 +54,9 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
