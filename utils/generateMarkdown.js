fc52789deb22cd06770d6f202d4a81b36a0c0722
astro <angelica.strong0725@gmail.com>
2023-09-27 10:15:37 -0700

    added license generator function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -34,7 +34,14 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None' || !renderLicenseLink(license)) {
+    return '';
+  }
+return `## License
+
+This project is licensed under the ${license} license. For more details, see [this link](${renderLicenseLink(license)}).`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
